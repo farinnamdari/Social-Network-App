cca61abe7ac988d6c91307fb88191af907f1dd23
Farin <farinnamdari@gmail.com>
2017-12-12 09:28:24 -0800

    Completed setting up Firebase integration

---
+++ SocialNetwrokApp/SingInVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SocialNetwrokApp
+//
+//  Created by Fareen on 12/12/17.
+//  Copyright © 2017 Fareen. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
+
