4856596b99678e33e1647a6746b8cc0c78257324
Farin <farinnamdari@gmail.com>
2017-12-18 09:45:56 -0800

    Completed news feed ui

--- SocialNetwrokApp/CircleView.swift
+++ SocialNetwrokApp/CircleView.swift
@@ -1,26 +1,28 @@
 //
-//  CustomView.swift
+//  CircleView.swift
 //  SocialNetwrokApp
 //
-//  Created by Fareen on 12/13/17.
+//  Created by Fareen on 12/18/17.
 //  Copyright © 2017 Fareen. All rights reserved.
 //
 
 import UIKit
 
-class CustomView: UIView {
+class CircleView: UIImageView {
     
     override func awakeFromNib() {
         super.awakeFromNib()
         
-        // setup shadow
         layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
         layer.shadowOpacity = 0.8
         layer.shadowRadius = 5.0
         layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
         
-        // setup view's corners
-        layer.cornerRadius = 2.0
+        layer.cornerRadius = self.frame.width / 2
     }
 
 }
