ce6e5b1406ddff7034b73e39fb9d3f8e0a393988
Farin <farinnamdari@gmail.com>
2017-12-19 10:59:22 -0800

    Completed parsing Firebase data

--- SocialNetwrokApp/FeedVC.swift
+++ SocialNetwrokApp/FeedVC.swift
@@ -14,14 +14,28 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var newsFeedTableView: UITableView!
     
+    var posts = [Post]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         newsFeedTableView.delegate = self
         newsFeedTableView.dataSource = self
         
+        // geting data from firebase
         DataService.ds.REF_POSTS.observe(.value, with: { (snapshot) in
-            print(snapshot.value)
+            if let snapshot = snapshot.children.allObjects as? [DataSnapshot] {
+                for snap in snapshot {
+                    if let postDict = snap.value as? Dictionary<String, AnyObject> {
+                        let key = snap.key
+                        let post = Post(postKey: key, postData: postDict)
+                        
+                        self.posts.append(post)
+                    }
+                }
+            }
+            
+            self.newsFeedTableView.reloadData()
         })
     }
     
@@ -31,10 +45,14 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return posts.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let post = posts[indexPath.row]
+        
+        print(post.caption)
+        
         return newsFeedTableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
     }
     
